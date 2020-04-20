2d4b85a5d869578e603a41cccd615a9e3a8b35fa
Mane897 <maneduron91@gmail.com>
2020-04-20 13:36:11 -0700

    Cambios

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -9,4 +9,35 @@ export class HomePage {
 
   constructor() {}
 
+  instrumentos = [
+    {
+      nombre: 'flauta',
+      imagen: '/assets/imagenes/flauta.png',
+      audio: '/assets/sonidos/flauta.mp3'
+    },
+    {
+      nombre: 'flauta',
+      imagen: '/assets/imagenes/platillos.png',
+      audio: '/assets/sonidos/platillos.mp3'
+    },
+    {
+      nombre: 'flauta',
+      imagen: '/assets/imagenes/bombo1.png',
+      audio: '/assets/sonidos/bombo1.mp3'
+    },
+    {
+      nombre: 'flauta',
+      imagen: '/assets/imagenes/bombo2.png',
+      audio: '/assets/sonidos/bombo2.mp3'
+    }
+  ];
+
+  reproducirSonido(instrumento) {
+    const sonido = new Audio();
+
+    sonido.src = instrumento.audio;
+
+    sonido.load();
+    sonido.play();
+  }
 }
